38c561fa397c3e21117539a9558367918a842319
ibtosmlin <ibt.osm@gmail.com>
2020-04-05 13:26:12 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,10 +6,10 @@ cp -i ./templates/*.* ~/work/atc/templates
 
 # atc bash for ~/.bashrc
 touch ~/.bashrc
-echo '# atc alias' >> ~/.bash
-echo 'alias atcd="source atc.sh d"' >> ~/.bash
-echo 'alias atcd="source atc.sh s"' >> ~/.bash
-echo 'alias atcd="source atc.sh t"' >> ~/.bash
-echo 'alias atcd="source atc.sh c"' >> ~/.bash
-echo 'alias atcd="source atc.sh l"' >> ~/.bash
-echo 'alias atcd="source atc.sh z"' >> ~/.bash
+echo '# atc alias' >> ~/.bashrc
+echo 'alias atcd="source atc.sh d"' >> ~/.bashrc
+echo 'alias atcs="source atc.sh s"' >> ~/.bashrc
+echo 'alias atct="source atc.sh t"' >> ~/.bashrc
+echo 'alias atcc="source atc.sh c"' >> ~/.bashrc
+echo 'alias atcl="source atc.sh l"' >> ~/.bashrc
+echo 'alias atcz="source atc.sh z"' >> ~/.bashrc
