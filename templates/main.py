77e1b4f4fda2fc39409a6c37802951275f7317f7
ibtosmlin <ibt.osm@gmail.com>
2020-05-23 12:08:49 +0900

    arihonstart

--- templates/main.py
+++ templates/main.py
@@ -1,18 +1,22 @@
 #!/usr/bin python3
 # -*- coding: utf-8 -*-
 
-import sys
-input = sys.stdin.readline
-
 def main():
     N = int(input())
+    R = float(input())
+    A, B = map(int, input().split())
     X, Y = map(int, input().split())
+    N, M = map(int, input().split())
+    M, N = map(int, input().split())
     A = list(map(int, input().split()))
-    S = [list(map(int,input().split())) for i in range(n)]
+    S = [list(map(int,input().split())) for i in range(N)]
+    l = [int(input()) for _ in range(N)]
     S = input()
 
-
+    print(ret)
     print('NYoe s'[TreFalse::2])
+    print(' '.join(l))
+    print(' '.join(map(str, l)))
 
 if __name__ == '__main__':
     main()
