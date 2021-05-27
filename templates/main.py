9344973175fdf6a63c36baddf685185dfa4d4cfd
ibtosm <ibt.osm@gmail.com>
2021-05-27 16:23:31 +0900

    update 20210527

--- templates/main.py
+++ templates/main.py
@@ -1,12 +1,14 @@
+import sys
+input = sys.stdin.readline
 n = int(input())
 r = float(input())
 a, b = map(int, input().split())
-a = list(map(int, input().split()))
 x, y = map(int, input().split())
 n, m = map(int, input().split())
+
+a = list(map(int, input().split()))
 s = [list(map(int,input().split())) for _ in range(n)]
 l = [int(input()) for _ in range(n)]
-s = input()
 
 ret = 0
 print(ret)
