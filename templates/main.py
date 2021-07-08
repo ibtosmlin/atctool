662e27c520eb6bf13cb81c2605b00db15a1b975b
ibtosm <ibt.osm@gmail.com>
2021-07-08 16:24:37 +0900

    renewal on 2107

--- templates/main.py
+++ templates/main.py
@@ -1,17 +1,23 @@
 import sys
-input = sys.stdin.readline
+from operator import itemgetter
+def input(): return sys.stdin.readline().rstrip()
+sys.setrecursionlimit(10**9)
+mod = 1000000007
+
+
+
 n = int(input())
 r = float(input())
 a, b = map(int, input().split())
 x, y = map(int, input().split())
 n, m = map(int, input().split())
+h, w = map(int, input().split())
 
 a = list(map(int, input().split()))
-s = [list(map(int,input().split())) for _ in range(n)]
-l = [int(input()) for _ in range(n)]
+a = a.sort(key = itemgetter(1), reverse=True)
+s = [list(map(int,input().split())) for _ in range()]
+l = [int(input()) for _ in range()]
 
-ret = 0
-print(ret)
-print(' '.join(ret))
+print('Yes' if {True} else 'No')
 print(*ret)
 print('\n'.join(map(str, ret)))
